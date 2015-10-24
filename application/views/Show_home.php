442026516ff06ca5449ca2b3d4751ca20ec7b6d8
adilbk <youpel.gerant@gmail.com>
2015-10-25 00:55:38 +0100

    description

--- application/views/Show_home.php
+++ application/views/Show_home.php
@@ -20,7 +20,7 @@
 					<article class="feature right">
 						<span class="image"><img src="<?php echo base_url(); ?>images/pic01.jpg"  alt="" /></span>
 						<div class="content">
-							<h2>Integer vitae libero acrisus egestas placerat  sollicitudin</h2>
+							<h2>Les services qualitées ? ne cherchez pas ailleurs</h2>
 							<p>Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est.</p>
 							<ul class="actions">
 								<li>
